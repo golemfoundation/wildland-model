f776cfae6f174872029172692bd2f223a9af5783
Joanna Rutkowska <joanna@golem.foundation>
2019-12-08 19:16:05 +0100

    storage backends: add request_*() methods to simualte actual interaction with client
    As noted in comments, in actual implemention the backend will not need to do any kind of graph traversals -- as it would just return a (static) file -- which will turn out to be either a container manifest or a blob with the actual content.

--- storage.py
+++ storage.py
@@ -4,7 +4,7 @@ from globals import *
 from edge_types import EdgeType
 from logger import Logger
 from ns_graph import NameSpaceNode, NameSpacePath, NameSpace
-from utils import wl_resolve_recursively
+from utils import wl_resolve_recursively, split_path_into_tokens
 
 class BackendStorage:
     """A specific storage backend (e.g. specific S3 bucket or WebDAV URL)
@@ -32,6 +32,51 @@ class BackendStorage:
 
     def __repr__ (self):
         return f"bknd_storage_{self.type}_{self.friendly_name}"
+
+    # This simulates normal, Wildland-agnostic request for a blob (file):
+    def request_raw_data (self, request):
+        Logger.log (f"{self}: got REQUEST for blob: {request}", icon='B')
+            
+    # Note: in real implentation the backend will not need to do any resolve
+    # since it would just return the requested file/key -- but as we're
+    # not using any backing for storage it needs to actually do the
+    # graph trabersing
+    
+    def request_resolve (self, request):
+        
+        Logger.log (f"{self}: got REQUEST: {request}", icon='B')
+        # Lets actually find what the backend would normaly return
+        # Of course in practical scenario, the backend would not have
+        # a full view of the Graph. And might be as simple as a static
+        # file server of key-dir server. 
+        path_as_list = split_path_into_tokens(request)
+
+        Logger.log (f"{self}: traversing the NameSpace tree: (ONLY in Simulation!) ", 'B')
+        Logger.nest_up()
+        prev_node = '@namespace'
+        for token in path_as_list:
+            next_node = None
+            for node in g_wlgraph.successors(prev_node):
+                if node.name == token:
+                    next_node = node
+                    break
+            if next_node is None:
+                raise LookupError
+            Logger.log (f"{node.name}", 'B')
+            prev_node = node
+        Logger.nest_down()
+        containers = g_wlgraph.successors(prev_node)
+        Logger.log (f"{self}: found {len(containers)} container(s):"
+                f" - {[c for c in containers]}", 'B')
+        container = containers[0] # TODO: support more
+        Logger.log (f"{self}: using 1st from the list: {container}", 'B')
+        if container.original_wlm is not None:
+            Logger.log (f"{self}: using the original container: {container.original_wlm}", 'B')
+            container = container.original_wlm
+        Logger.log (f"{self}: RESPONSE: {container}.yaml (incl. storage manifest(s))", 'B')
+        return container
+        
+        
 
 class BackendStorageWildland (BackendStorage):
     """A storage implemented on top of a Wildland container. Turtles..."""
