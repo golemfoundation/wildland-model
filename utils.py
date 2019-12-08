8219dc67de1d5405335d06d76caefdb85afe0d84
Joanna Rutkowska <joanna@golem.foundation>
2019-12-08 19:16:54 +0100

    utils: resolve: use newly introduced backend interface mockups

--- utils.py
+++ utils.py
@@ -18,45 +18,14 @@ def wl_resolve_single (wlm_actor, path):
     """Find container within The Wildland NameSpace."""
 
     Logger.log (f"wl_resolve_single: {wlm_actor.id}:{path}")
-    Logger.nest_up()
     verify_path(path)
     full_path = f"{wlm_actor.id}{path}"
-    path_as_list = split_path_into_tokens(full_path)
 
-    # Traverse the NameSpace graph along the 'path', starting from '@namespace':
-    Logger.nest_up()
-    Logger.log (f"traversing the NameSpace tree: ")
-    Logger.nest_up()
-    prev_node = '@namespace'
-    for token in path_as_list:
-        next_node = None
-        for node in g_wlgraph.successors(prev_node):
-            if node.name == token:
-                next_node = node
-                break
-        if next_node is None:
-            raise LookupError
-        Logger.log (f"{node.name}")
-        prev_node = node
-    Logger.nest_down()
-    Logger.nest_down()
-    containers = g_wlgraph.successors(prev_node)
-    Logger.log (f"found {len(containers)} container(s):"
-            f" - {[c for c in containers]}")
-    container = containers[0] # TODO: support more
-    Logger.log (f"using 1st from the list: {container}")
-    if container.original_wlm is not None:
-        Logger.log (f"using the original container: {container.original_wlm}")
-        container = container.original_wlm
-    wlm_storage = container.storage_manifests[0] # TODO: support more
-    bknd = wlm_storage.bknd_storage_backend
-    Logger.log (f"backend: {bknd}")
-    Logger.log (f"requesting: hash({path}/wildland.yaml) from {bknd}")
-    Logger.nest_down()
-    return container
+    bknd = wlm_actor.get_backends()[0] # TODO: support more
+    return bknd.request_resolve (full_path)
 
 def wl_resolve_recursively (wlm_actor_root, path):
-    Logger.log (f"resolving full path: {path}", icon='R')
+    Logger.log (f"resolving full path: {path}", icon='r')
     Logger.nest_up()
     pubkey_token,path_token = split_addr_into_actor_and_path (path)
     # Logger.log (f"--> pubkey_token: {pubkey_token}, path_token: {path_token}")
